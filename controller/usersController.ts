19b740d482e045dece9b16647b6aed3a94b23d0a
育祥 <a468223@gmail.com>
2023-04-24 23:35:11 +0900

    fix O-1-3 修改使用者API和O-1-4 刪除使用者API修改完成

--- controller/usersController.ts
+++ controller/usersController.ts
@@ -21,9 +21,13 @@ export const users = {
         return next(appError(400, Message.PAGE_NEED_IN_NUMBER, next));
       }
 
-      // 進入User跟Member Collection獲取沒被刪除的資料
-      const users = await User.find({ isDeleted: false }).sort({ titleNo: 1 });
-      const members = await Member.find({ isDeleted: false });
+      // 進入User跟Member Collection獲取沒被刪除的資料，並由舊到新排列
+      const users = await User.find({ isDeleted: false }).sort({
+        createdAt: "asc",
+      });
+      const members = await Member.find({ isDeleted: false }).sort({
+        createdAt: "asc",
+      });
 
       // 店家人員中取出需要的資訊
       const clerksList = users.map(
@@ -98,11 +102,10 @@ export const users = {
   // O-1-2 新增使用者API
   creatUser: handleErrorAsync(
     async (req: Request, res: Response, next: NextFunction) => {
-      // 姓名 電話 職位(預設給 4 ) 密碼
+      // 姓名 電話 職位 密碼
       const { name, phone, titleNo, isDisabled, password } = req.body;
       // title
       const titleMap: string[] = ["", "店長", "店員", "廚師", "會員"];
-      const numberMap: string[] = ["", "A", "B", "C", "D"];
 
       let errorMsgArray = [];
       // 驗證
@@ -146,13 +149,12 @@ export const users = {
       }
 
       const title = titleMap[titleNo];
-      const numberStr = numberMap[titleNo];
 
       // 編號依照職位種別去計算目前有幾筆該職位種別並+1
       const autoIncrementIndex =
         titleNo != 4
-          ? await autoIncrement(User, numberStr)
-          : await autoIncrement(Member, numberStr);
+          ? await autoIncrement(User, "A")
+          : await autoIncrement(Member, "B");
 
       // 職位代號不是4的話，在user collection新增，如果是4表示是會員，在member collection新增
       if (titleNo != 4) {
@@ -177,7 +179,145 @@ export const users = {
           number: autoIncrementIndex,
         });
       }
-      handleSuccess(res, "新增成功", null);
+      handleSuccess(res, Message.CREATE_SUCCESS, null);
+    }
+  ),
+  // O-1-3 修改使用者API
+  updateUser: handleErrorAsync(
+    async (req: any, res: Response, next: NextFunction) => {
+      const userId: string = req.params.id;
+      const hasRightUser = await User.findById(userId)
+        .where("isDeleted")
+        .ne(true);
+
+      if (hasRightUser === null) {
+        return next(appError(400, Message.ID_NOT_FOUND, next));
+      }
+      // 姓名 電話 職位 密碼
+      const { name, phone, titleNo, isDisabled, password } = req.body;
+      // title
+      const titleMap: string[] = ["", "店長", "店員", "廚師", "會員"];
+
+      let errorMsgArray = [];
+      // 驗證
+      if (!name) {
+        errorMsgArray.push(Message.NEED_INPUT_NAME);
+      }
+      if (!phone || !validator.isMobilePhone(phone, "zh-TW")) {
+        errorMsgArray.push(Message.NEED_INPUT_PHONE);
+      }
+      // 如果不是會員的職位代號的話需要驗證密碼
+      if (titleNo != 4) {
+        if (!password || !validator.isLength(password, { min: 8 })) {
+          errorMsgArray.push(Message.NEED_INPUT_PASSWORD);
+        }
+      }
+
+      if (!validator.isInt(titleNo.toString(), { min: 1, max: 4 })) {
+        errorMsgArray.push(Message.NEED_INPUT_TITLENO);
+      }
+
+      // 會員與店家人員不可直接透過職位代號轉換()
+      if (
+        (hasRightUser.titleNo != 4 && titleNo == 4) ||
+        (hasRightUser.titleNo == 4 && titleNo != 4)
+      ) {
+        errorMsgArray.push(Message.TITLENO_TRANSFER_ERROR);
+      }
+
+      // 如果電話號碼與原本不同
+      if (phone != hasRightUser.phone) {
+        let hasSamePhone = null;
+        // 依照職位代號去相對應的collection搜尋是否已有相同電話號碼
+        if (hasRightUser.titleNo != 4) {
+          hasSamePhone = await User.findOne({ phone });
+        } else {
+          hasSamePhone = await Member.findOne({ phone });
+        }
+
+        if (hasSamePhone != null) {
+          errorMsgArray.push(Message.SAME_PHONE_REGISTERED);
+        }
+      }
+
+      if (!(isDisabled === true || isDisabled === false)) {
+        errorMsgArray.push(Message.NEED_INPUT_STATUS);
+      }
+
+      // 如果有錯誤訊息有返回400
+      if (errorMsgArray.length > 0) {
+        return next(appError(400, errorMsgArray.join(";"), next));
+      }
+
+      interface IUserParam {
+        name: string;
+        phone: string;
+        password?: string;
+        titleNo: number;
+        title: string;
+        isDisabled: boolean;
+        revisedAt: string;
+      }
+
+      const title = titleMap[titleNo];
+
+      const params: IUserParam = {
+        name,
+        phone,
+        titleNo,
+        title,
+        isDisabled,
+        revisedAt: isoDate(),
+      };
+
+      // 職位代號不是4的話，在user collection新增，如果是4表示是會員，在member collection更新
+      if (titleNo != 4) {
+        if (password) {
+          // 加密
+          const bcryptPassword = await bcrypt.hash(password, 12);
+          params.password = bcryptPassword;
+        }
+        await User.findByIdAndUpdate(userId, params);
+      } else {
+        await Member.findByIdAndUpdate(userId, params);
+      }
+
+      handleSuccess(res, Message.REVISE_SUCCESS, null);
+    }
+  ),
+  // O-1-4 刪除使用者API
+  softDeleteUser: handleErrorAsync(
+    async (req: any, res: Response, next: NextFunction) => {
+      const userId = req.params.id;
+      const titleNo = req.params.titleNo;
+      // 排除已經被軟廚的人員
+      let hasRightUser = {} || null;
+
+      if (titleNo != 4) {
+        hasRightUser = await User.findById(userId).where("isDeleted").ne(true);
+      } else {
+        hasRightUser = await Member.findById(userId)
+          .where("isDeleted")
+          .ne(true);
+      }
+
+      if (hasRightUser === null) {
+        return next(appError(400, Message.ID_NOT_FOUND, next));
+      }
+
+      const deleteItem = {
+        isDeleted: true,
+        deletedAt: isoDate(),
+      };
+      if (titleNo != 4) {
+        await User.findByIdAndUpdate(userId, deleteItem);
+        // 有需要可用returnDocument返回更新的data
+        // { returnDocument: "after", }
+      } else {
+        await Member.findByIdAndUpdate(userId, deleteItem);
+      }
+
+      handleSuccess(res, Message.DELETE_SUCCESS, null);
     }
   ),
   signUp: handleErrorAsync(
@@ -307,106 +447,6 @@ export const users = {
         return next(appError(400, "查無使用者", next));
       }
       handleSuccess(res, "成功", hasRightUser);
-    }
-  ),
-  updateUser: handleErrorAsync(
-    async (req: any, res: Response, next: NextFunction) => {
-      const userId: string = req.params.id;
-      const hasRightUser = await User.findById(userId)
-        .where("isDeleted")
-        .ne(true);
-
-      if (hasRightUser === null) {
-        return next(appError(400, "查無使用者", next));
-      }
-
-      const {
-        name,
-        phone,
-        titleNo = 4,
-        password,
-        isDisabled = true,
-      } = req.body;
-      // todo
-      // title
-      const titleMap: string[] = ["", "店長", "店員", "廚師", "會員"];
-
-      // name 不得為空
-      interface IUserParam {
-        name: string;
-        phone: string;
-        password?: string;
-        titleNo: number;
-        title: string;
-        isDisabled: boolean;
-        revisedAt: string;
-      }
-      // 驗證
-      if (!name || !phone || !titleNo) {
-        return next(appError(400, "欄位不可為空", next));
-      }
-
-      if (password !== undefined && !validator.isLength(password, { min: 8 })) {
-        return next(appError(400, "密碼長度需大於 8 碼", next));
-      }
-
-      if (!validator.isLength(phone, { min: 8 })) {
-        return next(appError(400, "電話長度需大於 8 碼", next));
-      }
-
-      if (!validator.isInt(titleNo.toString(), { min: 1, max: 4 })) {
-        return next(appError(400, "職位有誤", next));
-      }
-
-      const hasSamePhone = await User.findOne({ phone });
-      if (hasRightUser.phone !== phone && hasSamePhone !== null) {
-        return next(appError(400, "電話重複", next));
-      }
-
-      const title = titleMap[titleNo];
-      const params: IUserParam = {
-        name,
-        phone,
-        titleNo,
-        title,
-        isDisabled,
-        revisedAt: isoDate(),
-      };
-      if (password) {
-        const bcryptPassword = await bcrypt.hash(password, 12);
-        params.password = bcryptPassword;
-      }
-      const updateUser = await User.findByIdAndUpdate(userId, params, {
-        returnDocument: "after",
-      });
-
-      handleSuccess(res, "成功", updateUser);
-    }
-  ),
-  softDeleteUser: handleErrorAsync(
-    async (req: any, res: Response, next: NextFunction) => {
-      const userId = req.params.id;
-      // 排除已經被軟廚的人員
-      const hasRightUser = await User.findById(userId)
-        .where("isDeleted")
-        .ne(true);
-
-      if (hasRightUser === null) {
-        return next(appError(400, "查無使用者", next));
-      }
-
-      await User.findByIdAndUpdate(
-        userId,
-        {
-          isDeleted: true,
-          deletedAt: isoDate(),
-        },
-        {
-          returnDocument: "after",
-        }
-      );
-
-      handleSuccess(res, "刪除成功", null);
     }
   ),
 };
--- routes/users.ts
+++ routes/users.ts
@@ -6,12 +6,12 @@ const router = express.Router();
 /* GET users listing. */
 router.get("/", isOwnerAuth, usersController.getUsers);
 router.post("/", isOwnerAuth, usersController.creatUser);
-router.delete("/:id", isAuth, usersController.softDeleteUser);
+router.patch("/:id", isOwnerAuth, usersController.updateUser);
+router.delete("/:id/:titleNo", isOwnerAuth, usersController.softDeleteUser);
 router.post("/sign_up", usersController.signUp);
 router.post("/login", usersController.login);
 router.post("/reset_password", isAuth, usersController.resetPassword);
 router.get("/profile", isAuth, usersController.profile);
 router.get("/:id", isAuth, usersController.getUser);
-router.patch("/:id", isAuth, usersController.updateUser);
 
 export default router;
