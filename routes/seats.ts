1268d8c1cd179754b34ff7c13901f358639ba673
eric <howcrazys@gmail.com>
2023-04-28 06:03:05 +0800

    fix: 修正座位為店長權限

--- routes/seats.ts
+++ routes/seats.ts
@@ -1,15 +1,15 @@
 import seatsController from "../controller/seatsController";
 import express from "express";
-import { isAuth } from "../middleware/auth";
+import { isOwnerAuth } from "../middleware/auth";
 const router = express.Router();
 
-router.get("/", isAuth, seatsController.getSeats);
-router.post("/", isAuth, seatsController.createSeat);
-router.patch("/:tableNo", isAuth, seatsController.patchSeat);
-router.delete("/:tableNo", isAuth, seatsController.softDeleteSeat);
+router.get("/", isOwnerAuth, seatsController.getSeats);
+router.post("/", isOwnerAuth, seatsController.createSeat);
+router.patch("/:tableNo", isOwnerAuth, seatsController.patchSeat);
+router.delete("/:tableNo", isOwnerAuth, seatsController.softDeleteSeat);
 
-router.get("/table-code", isAuth, seatsController.getTableCode);
-router.post("/table-code", isAuth, seatsController.createTableCode);
-router.delete("/table-code/:seatsType", isAuth, seatsController.deleteTableCode);
+router.get("/table-code", isOwnerAuth, seatsController.getTableCode);
+router.post("/table-code", isOwnerAuth, seatsController.createTableCode);
+router.delete("/table-code/:seatsType", isOwnerAuth, seatsController.deleteTableCode);
 
 export default router;
