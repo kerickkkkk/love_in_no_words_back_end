fdcc413e4f97320ce732da369baaa66d8d9a4534
eric <howcrazys@gmail.com>
2023-04-27 22:44:41 +0800

    feat: collect 會用到的先出一版

---
+++ models/productTypeModel.ts
@@ -0,0 +1,52 @@
+import { Schema, Document, model } from "mongoose";
+interface ProductType extends Document {
+  productsType: number;
+  productsTypeName: number;
+  createdAt: Date;
+  isDisabled: boolean;
+  revisedAt?: Date;
+  isDeleted: boolean;
+  deletedAt?: Date;
+}
+
+const productTypeSchema = new Schema(
+  {
+    productsType: {
+      type: Number,
+      required: [true, "請輸入商品分類編號"],
+    },
+    productsTypeName: {
+      type: Number,
+      required: [true, "請輸入商品分類名稱"],
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+    isDisabled: {
+      type: Boolean,
+      required: [true, "狀態請以布林值形式輸入"],
+      default: false,
+    },
+    revisedAt: {
+      type: Date,
+      default: null,
+      select: false,
+    },
+    isDeleted: {
+      type: Boolean,
+      default: false,
+      select: false,
+    },
+    deletedAt: {
+      type: Date,
+      select: false,
+    },
+  },
+  { 
+    versionKey: false, 
+    collection: "productType" 
+  }
+);
+
+export default model<ProductType>("ProductType", productTypeSchema);
