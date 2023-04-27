4a5cfd1202503740da86c244e275c9f6f5a1b557
eric <howcrazys@gmail.com>
2023-04-27 21:47:24 +0800

    feat: socket.io 初步版本

--- routes/index.ts
+++ routes/index.ts
@@ -6,4 +6,12 @@ router.get('/',  (req: Request, res: Response): void => {
   res.render('index', { title: '歡迎來到傲嬌甜點店' });
 });
 
+
+router.get('/socket',  (req: Request, res: Response): void => {
+  const io = req.app.settings.io;
+  console.log('準備廣播囉')
+  io.emit('employee', '從 route 打 出去employee')
+  res.end()
+});
+
 export default router;
