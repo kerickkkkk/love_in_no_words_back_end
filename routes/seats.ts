73549eaaf39ded257677cafca4b43af25dc5bd67
育祥 <a468223@gmail.com>
2023-04-30 00:07:43 +0900

    fix seat路徑復原

--- routes/seats.ts
+++ routes/seats.ts
@@ -8,6 +8,13 @@ router.get("/", isOwnerAuth, seatsController.getSeats);
 router.post("/", isOwnerAuth, seatsController.createSeat);
 router.patch("/:tableNo", isOwnerAuth, seatsController.patchSeat);
 router.delete("/:tableNo", isOwnerAuth, seatsController.softDeleteSeat);
+router.get("/table-code", isAuth, seatsController.getTableCode);
+router.post("/table-code", isAuth, seatsController.createTableCode);
+router.delete(
+  "/table-code/:seatsType",
+  isAuth,
+  seatsController.deleteTableCode
+);
 
 router.post("/reservation", isAuth, reservationController.createReservation);
 export default router;
