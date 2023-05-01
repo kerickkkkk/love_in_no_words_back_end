b08573c8c79f5047781bff6799f094c3bde3d988
eric <howcrazys@gmail.com>
2023-05-01 17:16:11 +0800

    feat: 調整功能

--- utils/modelsExtensions.ts
+++ utils/modelsExtensions.ts
@@ -3,10 +3,12 @@ import { Model } from "mongoose";
 // 傳入該 model 前面戴上前綴 總共十碼（含前綴）
 export const autoIncrement = async (
   model: Model<any>,
-  word: string
+  word: string,
+  columnType?: string | undefined
 ): Promise<string> => {
-  // 去collection尋找符合number開頭為${word}的筆數
-  const num = await model.countDocuments({ number: { $regex: `${word}.*` } });
+  // 去collection尋找符合 傳入欄位名稱如果沒有就傳 number 開頭為${word}的筆數
+  const columnName = columnType || 'number'
+  const num = await model.countDocuments({ [columnName]: { $regex: `${word}.*` } });
   let tempNum = 0;
   if (num >= 1) {
     tempNum = num + 1;
@@ -23,7 +25,8 @@ export const autoIncrementNumber = async (
   columnName = ""
 ): Promise<number> => {
   // 因為排序是全部 需無視 isDisabled, isDeleted
-  const lastObj = await model.findOne().sort({ createdAt: -1 })
-  const index = lastObj !== null ? lastObj[columnName] + 1 : 1
+  // const lastObj = await model.findOne().sort({ createdAt: -1 })
+  // const index = lastObj !== null ? lastObj[columnName] + 1 : 1
+  const index = await model.countDocuments() + 1
   return index
 };
