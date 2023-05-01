edb0256a3a1b6dfb590daaea2c11acbe33cb9b3f
eric <howcrazys@gmail.com>
2023-05-01 17:15:47 +0800

    feat: 調整 tableName, 將tablecode 功能保留

--- routes/seats.ts
+++ routes/seats.ts
@@ -8,13 +8,14 @@ router.get("/", isOwnerAuth, seatsController.getSeats);
 router.post("/", isOwnerAuth, seatsController.createSeat);
 router.patch("/:tableNo", isOwnerAuth, seatsController.patchSeat);
 router.delete("/:tableNo", isOwnerAuth, seatsController.softDeleteSeat);
-router.get("/table-code", isAuth, seatsController.getTableCode);
-router.post("/table-code", isAuth, seatsController.createTableCode);
-router.delete(
-  "/table-code/:seatsType",
-  isAuth,
-  seatsController.deleteTableCode
-);
+// 保留座位功能 改成直接寫入 seats 座位人數上限
+// router.get("/table-code", isAuth, seatsController.getTableCode);
+// router.post("/table-code", isAuth, seatsController.createTableCode);
+// router.delete(
+//   "/table-code/:seatsType",
+//   isAuth,
+//   seatsController.deleteTableCode
+// );
 
 router.get("/table-code", isOwnerAuth, seatsController.getTableCode);
 router.post("/table-code", isOwnerAuth, seatsController.createTableCode);
