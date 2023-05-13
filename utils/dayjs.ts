b21a36f05fad05e48fd0d3d0423f601218eab76a
育祥 <a468223@gmail.com>
2023-05-13 10:31:40 +0900

    fix 大量製造訂單測試API 修改為可以輸入月份隨機生成不同日期時間的訂單

--- utils/dayjs.ts
+++ utils/dayjs.ts
@@ -3,13 +3,13 @@ import isLeapYear from "dayjs/plugin/isLeapYear";
 import utc from "dayjs/plugin/utc";
 import timezone from "dayjs/plugin/timezone";
 import "dayjs/locale/zh-tw"; // import locale
-import customParseFormat from "dayjs/plugin/customParseFormat"
+import customParseFormat from "dayjs/plugin/customParseFormat";
 
 dayjs.locale("zh-tw"); // use locale
 dayjs.extend(isLeapYear); // use plugin
 dayjs.extend(utc);
 dayjs.extend(timezone);
-dayjs.extend(customParseFormat)
+dayjs.extend(customParseFormat);
 
 export const isoDate = (): string =>
   dayjs().utcOffset(8).format("YYYY-MM-DDTHH:mm:ss.SSS") + "Z";
@@ -19,9 +19,28 @@ export const slashDate = (date: Date): string =>
 // 判斷系統當下時段 上午 下午 晚上
 // 晚上時間會變成晚上 暫時先用非上午 就是下午
 export const period = (): string =>
-  (dayjs().utcOffset(8).format("A") === "上午" ? "上午" : "下午")
+  dayjs().utcOffset(8).format("A") === "上午" ? "上午" : "下午";
 
 export const combinedDateTimeString = (): string =>
   dayjs().utcOffset(8).format("YYYYMMDDHHmmss");
+
+export const randomDateTimeString = (month: number): string => {
+  // 計算隨機生成的月份的最大日期
+  const maxDate = dayjs()
+    .set("month", month - 1)
+    .endOf("month")
+    .date();
+
+  // 生成介於1和最大日期之間的隨機整數
+  const randomDate = Math.floor(Math.random() * maxDate) + 1;
+
+  // 設置日期物件的月份和日期
+  const dateObject = dayjs()
+    .set("month", month - 1)
+    .set("date", randomDate);
+  const timeStamp = dateObject.utcOffset(8).format("YYYYMMDDHHmmss");
+
+  return timeStamp;
+};
 
 export default dayjs;
