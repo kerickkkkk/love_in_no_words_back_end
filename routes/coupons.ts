4433910785b8649e5d56e572a285d72e25a6e0b7
eric <howcrazys@gmail.com>
2023-05-16 20:20:49 +0800

    feat:  路由 O-4-1

--- routes/coupons.ts
+++ routes/coupons.ts
@@ -1,9 +1,10 @@
 import couponsController from "../controller/couponsController";
 import express from "express";
-import { isOwnerAuth } from "../middleware/auth";
+import { isAuth, isOwnerAuth } from "../middleware/auth";
 const router = express.Router();
 
 // 管理端
+router.get("/", isAuth, couponsController.getCoupons);
 router.get("/admin", isOwnerAuth, couponsController.getCoupons);
 router.post("/admin", isOwnerAuth, couponsController.createCoupons);
 router.patch("/admin/:couponNo", isOwnerAuth, couponsController.patchCoupons);
