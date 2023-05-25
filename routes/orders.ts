f571d4227a734f0089339f6bb65e7edd76dae551
chris chen <christinachen149@gmail.com>
2023-05-25 09:41:43 +0800

    Add S-3-1 , S-3-3

--- routes/orders.ts
+++ routes/orders.ts
@@ -5,6 +5,8 @@ import ordersController from "../controller/ordersController"
 const router = express.Router();
 
 router.post("/", isAuth, ordersController.handleOrder);
-router.post("/calculate/total-price", isAuth, ordersController.handleOrder)
-
+router.post("/calculate/total-price", isAuth, ordersController.handleOrder);
+router.get("/", isAuth, ordersController.getOrders);
+router.get("/detail/:orderId", isAuth, ordersController.getOrderDetail);
+router.post("/rating/:orderId", isAuth, ordersController.postRating);
 export default router;
