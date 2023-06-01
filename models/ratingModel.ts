20462cea90ee52fb7b0404a618d5fdbf69001265
chris chen <christinachen149@gmail.com>
2023-06-01 22:00:43 +0800

    fixed S-3-3 提交評分資訊失敗

--- models/ratingModel.ts
+++ models/ratingModel.ts
@@ -1,21 +1,27 @@
-import { Schema, Document, model } from "mongoose";
-interface rating extends Document {
+import { Schema, Document, model, Types } from "mongoose";
+
+interface Rating extends Document {
   satisfaction: number;
   description: string;
+  order: Types.ObjectId; // Reference to the Order model
 }
+
 const ratingSchema = new Schema(
   {
     satisfaction: {
       type: Number,
       required: [true, "請填寫滿意度"],
-      default: 0,
     },
     description: {
       type: String,
       required: false,
     },
-  },
-  { timestamps: true }
+    order: {
+      type: Schema.Types.ObjectId,
+      ref: "Order",
+      required: true,
+    }
+  }
 );
 
-export default model<rating>("rating", ratingSchema);
+export default model<Rating>("Rating", ratingSchema);
