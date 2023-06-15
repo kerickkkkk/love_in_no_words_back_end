3a70c5cfaabbf2ac9e621f243fd26785da680792
eric <howcrazys@gmail.com>
2023-06-15 22:11:27 +0800

    feat : 測試 jest 修正非同步

--- connection/service/redis.ts
+++ connection/service/redis.ts
@@ -2,24 +2,24 @@ import { createClient } from 'redis';
 import dotenv from 'dotenv'
 dotenv.config();
 const redisUrl = process.env.REDIS_DATABASE;
-
-const redisClient = createClient({
-  url: redisUrl
-});
-
-// // 初始先是連一次 和 cache 分開因為有問題
-const connectRedis = async () => {
-  redisClient.on('error', (err: Error) => console.log(`Redis 錯誤 : ${err}`));
-  await redisClient.connect();
-  return 'Redis 連線成功';
-};
+let redisClient: any = null
 if (process.env.NODE_ENV !== 'test') {
+  redisClient = createClient({
+    url: redisUrl
+  });
+
+  // // 初始先是連一次 和 cache 分開因為有問題
+  const connectRedis = async () => {
+    redisClient.on('error', (err: Error) => console.log(`Redis 錯誤 : ${err}`));
+    await redisClient.connect();
+    return 'Redis 連線成功';
+  };
   connectRedis();
 }
 export const setCache = async (cacheKey: string, data: any) => {
   try {
     const key = btoa(cacheKey)
-    const res = await redisClient.set(key, JSON.stringify(data), { EX: 10 })
+    const res = redisClient && await redisClient.set(key, JSON.stringify(data), { EX: 10 })
   } catch (error) {
     console.log('Redis 有問題');
   }
@@ -28,7 +28,7 @@ export const setCache = async (cacheKey: string, data: any) => {
 export const checkCacheKeyExist = async (cacheKey: string) => {
   try {
     const key = btoa(cacheKey)
-    return await redisClient.get(key)
+    return redisClient && await redisClient.get(key)
   } catch (error) {
     console.log('Redis 有問題');
   }
