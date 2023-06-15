cb56721c22c835a94a73ba2bec5f2b1cb3c571df
eric <howcrazys@gmail.com>
2023-06-15 18:54:55 +0800

    feat : 關閉 redis linepay 測試

--- connection/service/redis.ts
+++ connection/service/redis.ts
@@ -13,7 +13,9 @@ const connectRedis = async () => {
   await redisClient.connect();
   return 'Redis 連線成功';
 };
-connectRedis();
+if (process.env.NODE_ENV !== 'test') {
+  connectRedis();
+}
 export const setCache = async (cacheKey: string, data: any) => {
   try {
     const key = btoa(cacheKey)
