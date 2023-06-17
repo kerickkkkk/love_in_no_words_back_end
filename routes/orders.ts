f0b8b07ea6fef0d6d8fd943a0bd126ffed0c2db3
eric <howcrazys@gmail.com>
2023-06-17 09:49:38 +0800

    feat: S-3-5 查詢訂單結帳狀態 (含測試

--- routes/orders.ts
+++ routes/orders.ts
@@ -11,6 +11,7 @@ router.post("/calculate/total-price", isAuth, ordersController.handleOrder);
 router.get("/", isAuth, ordersController.getOrders);
 router.get("/detail/:orderId", isAuth, ordersController.getOrderDetail);
 router.post("/rating/:orderId", isAuth, ordersController.postRating);
+router.get("/check/cash/:orderNo", isAuth, ordersController.checkCash);
 //chef
 router.get("/pick-up", isCookAuth, chefController.getPickUpOrders);
 router.patch("/pick-up/:orderNo", isCookAuth, chefController.updateOrderStatus);
